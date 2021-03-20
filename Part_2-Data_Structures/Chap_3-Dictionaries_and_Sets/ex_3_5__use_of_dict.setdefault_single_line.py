50f65293393b6d48ce93f3375c5260ebc27c4f90
Lucas Cavalcante <lucascpcavalcante@gmail.com>
2021-03-20 00:49:40 -0300

    pg.76

---
+++ Part_2-Data_Structures/Chap_3-Dictionaries_and_Sets/ex_3_5__use_of_dict.setdefault_single_line.py
@@ -0,0 +1,20 @@
+""" Build an index mapping word -> list of occurences """
+
+import sys
+import re
+import collections
+
+WORD_RE = re.compile('\w+')
+
+index = collections.defaultdict(list)
+with open(sys.argv[1], encoding='utf-8') as fp:
+  for line_no, line in enumerate(fp, 1):
+    for match in WORD_RE.finditer(line):
+      word = match.group()
+      column_no = match.start()+1
+      location = (line_no, column_no)
+      index[word].append(location)
+
+# print in alphabetical order
+for word in sorted(index, key=str.upper):
+  print(word, index[word])
