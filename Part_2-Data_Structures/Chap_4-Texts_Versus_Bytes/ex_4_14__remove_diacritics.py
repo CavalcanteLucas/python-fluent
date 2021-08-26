657631d3a9289d03dd574513e8c50817c6383a5e
Lucas Cavalcante <lucascpcavalcante@gmail.com>
2021-08-26 19:50:24 +0200

    pg.132
    pg.131

---
+++ Part_2-Data_Structures/Chap_4-Texts_Versus_Bytes/ex_4_14__remove_diacritics.py
@@ -0,0 +1,8 @@
+import unicodedata
+
+def shave_marks(txt):
+    """Remove all diacritcs marks"""
+    norm_txt = unicodedata.normalize('NFD', txt)
+    shaved = ''.join(c for c in norm_txt
+                        if not unicodedata.combining(c))
+    return unicodedata.normalize('NFC', shaved)
---
+++ Part_2-Data_Structures/Chap_4-Texts_Versus_Bytes/ex_4_17__asciize.py
@@ -0,0 +1,29 @@
+import unicodedata
+
+from ex_4_16__sanitize import shave_marks_latin
+
+single_map = str.maketrans("""‚ƒ„†ˆ‹‘’“”•–—˜›""",
+                           """'f"*^<''""---~>""")
+
+multi_map = str.maketrans({
+    '€': '<euro>',
+    '…': '...',
+    'Œ': 'OE',
+    '™': '(TM)',
+    'œ': 'oe',
+    '‰': '<per mille>',
+    '‡': '**',
+})
+
+multi_map.update(single_map)
+
+
+def dewinize(txt):
+    """Replace Win1252 symbols with ASCII chars or sequence"""
+    return txt.translate(multi_map)
+
+
+def asciize(txt):
+    no_marks = shave_marks_latin(dewinize(txt))
+    no_marks = no_marks.replace('ß', 'ss')
+    return unicodedata.normalize('NFKC', no_marks)
