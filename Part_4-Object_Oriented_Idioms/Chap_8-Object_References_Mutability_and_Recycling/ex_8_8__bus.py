c5734fc28bfc66ae8cc5b8bd739d2b3a108f6a45
Lucas Cavalcante <lucascpcavalcante@gmail.com>
2021-12-26 08:03:44 +0300

    pg.235

---
+++ Part_4-Object_Oriented_Idioms/Chap_8-Object_References_Mutability_and_Recycling/ex_8_8__bus.py
@@ -0,0 +1,13 @@
+class Bus:
+
+    def __init__(self, passengers=None):
+        if passengers is None:
+            self.passengers = []
+        else:
+            self.passengers = list(passengers)
+    
+    def pick(self, name):
+        self.passengers.append(name)
+    
+    def drop(self, name):
+        self.passengers.remove(name)
