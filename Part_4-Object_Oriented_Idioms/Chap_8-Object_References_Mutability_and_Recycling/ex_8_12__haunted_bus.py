3f6f074803b8bc7faed2d895e8eb721db4d50463
Lucas Cavalcante <lucascpcavalcante@gmail.com>
2022-01-13 12:42:50 +0300

    pg.246

---
+++ Part_4-Object_Oriented_Idioms/Chap_8-Object_References_Mutability_and_Recycling/ex_8_12__haunted_bus.py
@@ -0,0 +1,11 @@
+class HauntedBus:
+    """A bus model haunted by ghost passengers"""
+
+    def __init__(self, passengers=[]):
+        self.passengers = passengers
+    
+    def pick(self, name):
+        self.passengers.append(name)
+    
+    def drop(self, name):
+        self.passengers.remove(name)
