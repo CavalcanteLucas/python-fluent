d91b93e690077cc51ea36e71f097069d1f4293cc
Lucas Cavalcante <lucascpcavalcante@gmail.com>
2022-06-06 07:26:13 -0300

    write: pg.444

---
+++ Part_5__Control_Flow/Chap_14__Iterables_Iterators_and_Generators/ex_14_12__aritprog_gen.py
@@ -0,0 +1,8 @@
+def aritprog_gen(begin, step, end=None):
+    result = type(begin + step)(begin)
+    forever = end is None
+    index = 0
+    while forever or result < end:
+        yield result
+        index += 1
+        result = begin + step * index
