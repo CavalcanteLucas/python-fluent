85bd6fc40145bd5dd4b6fc0f360021334fdf66cc
Lucas Cavalcante <lucascpcavalcante@gmail.com>
2022-06-29 04:28:18 -0300

    read: 331

--- log.py
+++ log.py
@@ -1,5 +1,5 @@
 """
-read: 326
+read: 331
 """
 
 import argparse
@@ -10,25 +10,25 @@ TOTAL_PAGES = 711
 
 def main(current_page):
 
-    with open(__file__, "r") as file:
+    with open(__file__, 'r') as file:
         lines = file.readlines()
 
-    lines[1] = "read: {}\n".format(current_page)
+    lines[1] = 'read: {}\n'.format(current_page)
 
-    with open(__file__, "w") as file:
+    with open(__file__, 'w') as file:
         for line in lines:
             file.write(line)
 
     prog = int(current_page) / TOTAL_PAGES * 100
-    
+
     system('git add -A')
     system('git commit -m "{}"'.format(lines[1]))
     system('git push origin read')
 
-    print("{:.2f}".format(prog), end="")
+    print('{:.2f}'.format(prog), end='')
 
-    
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     p = argparse.ArgumentParser()
     p.add_argument('current_page')
     args = p.parse_args()
